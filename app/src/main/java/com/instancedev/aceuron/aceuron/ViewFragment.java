fbe5679693027dcf03a76a35e5a5fc632ee3be66
Raphael Göldner <you@example.com>
2017-06-01 19:25:00 +0200

    started ViewPager

---
+++ app/src/main/java/com/instancedev/aceuron/aceuron/ViewFragment.java
@@ -0,0 +1,21 @@
+package com.instancedev.aceuron.aceuron;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.instancedev.aceuron.aceuron.R;
+
+
+public class ViewFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.viewpager, container, false);
+
+        return rootView;
+    }
+}
