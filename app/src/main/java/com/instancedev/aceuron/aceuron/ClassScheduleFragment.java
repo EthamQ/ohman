ade16290ea3d1e13280af74a596e92a10454c282
Instance <->
2017-09-08 08:20:45 +0200

    Add ClassScheduleFragment and EditClassScheduleActivity

---
+++ app/src/main/java/com/instancedev/aceuron/aceuron/ClassScheduleFragment.java
@@ -0,0 +1,70 @@
+package com.instancedev.aceuron.aceuron;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ClassScheduleFragment extends Fragment {
+
+    View view;
+
+    GridView gridView;
+
+    public ClassScheduleFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle){
+        view = inflater.inflate(R.layout.fragment_class_schedule, container, false);
+        init();
+        return view;
+    }
+
+    public static ClassScheduleFragment newInstance(){
+        return new ClassScheduleFragment();
+    }
+
+    public void init(){
+        gridView = (GridView) view.findViewById(R.id.gridView);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // TODO open dialog that prompts for details or edit
+                // first ask on the bottom whether to show details or edit
+                switch (position) {
+                    case 0:
+                    case 6:
+                    case 12:
+                    case 18:
+                    case 24:
+                        // TODO don't prompt, those are weekday items
+                        break;
+                    default:
+                        // TODO prompt
+                }
+
+                // TODO hmm which one is better, switch or modulo
+
+                if (position % 6 == 0) {
+                    // TODO don't prompt
+                } else {
+                    // TODO prompt
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/instancedev/aceuron/aceuron/EditClassScheduleActivity.java
@@ -0,0 +1,16 @@
+package com.instancedev.aceuron.aceuron;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EditClassScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_class_schedule);
+    }
+}
