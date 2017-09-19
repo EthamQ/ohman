8e62702b375567639dd952cb127703a2b0166536
EthamQ <raphael.cx8@gmail.com>
2017-09-19 16:36:43 +0200

    Testcommit

--- app/src/main/java/com/instancedev/aceuron/aceuron/Main.java
+++ app/src/main/java/com/instancedev/aceuron/aceuron/Main.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
 
+    //test change
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
